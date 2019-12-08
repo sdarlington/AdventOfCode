ee18fb2794690e2c010c176b1fb4d0fe7c3771a5
Stephen Darlington <stephen@zx81.org.uk>
2019-12-08 11:49:27 +0000

    Day 8, part 2

--- 2019/day8/day8.scala
+++ 2019/day8/day8.scala
@@ -2,12 +2,33 @@
 
 case class Image (data : String, x : Int, y : Int) {
     def point(x : Int, y: Int, layer : Int) : Int = {
-      data( layer * this.x * this.y + y * this.y + x )
+      data( layer * this.x * this.y + y * this.x + x ) - '0'
     }
 
     def layer(l : Int) : Seq[Int] = data.slice(l * x * y, l * x * y + x * y).map { _.toInt - '0' }
 
     def layerCount : Int = data.length / (x * y)
+
+    def colourAtPoint(x : Int, y : Int) : Int = {
+        val p = for (l <- 0 until layerCount) yield point(x,y,l)
+        p.filter(_ != 2).head
+    }
+
+    def colourImage : Seq[Int] = {
+        for (y <- 0 until this.y; x <- 0 until this.x) yield colourAtPoint(x,y)
+    }
+
+    def displayColouredImage : Unit = {
+        val img = colourImage.map({
+            case 1=> "⬜"*2
+            case 0 => "⬛"*2
+            case 2 => "?"*2
+        })
+        for (row <- 0 until this.y) {
+          println(img.slice(row * x, row * x + x).mkString)
+          println(img.slice(row * x, row * x + x).mkString)
+        }
+    }
 }
 
 val inputFile = io.Source.fromFile("input.txt")
@@ -27,3 +48,5 @@ val calc = layer.filter(_ == 1).size * layer.filter(_ == 2).size
 
 println(s"Part 1\n$calc")
 
+println("Part 2")
+image.displayColouredImage
