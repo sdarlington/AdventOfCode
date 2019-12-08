94e1acfa9b0c579b63d39157e1451ed964dee7da
Stephen Darlington <stephen@zx81.org.uk>
2019-12-08 09:36:02 +0000

    Day 8 part 1

---
+++ 2019/day8/day8.scala
@@ -0,0 +1,29 @@
+
+
+case class Image (data : String, x : Int, y : Int) {
+    def point(x : Int, y: Int, layer : Int) : Int = {
+      data( layer * this.x * this.y + y * this.y + x )
+    }
+
+    def layer(l : Int) : Seq[Int] = data.slice(l * x * y, l * x * y + x * y).map { _.toInt - '0' }
+
+    def layerCount : Int = data.length / (x * y)
+}
+
+val inputFile = io.Source.fromFile("input.txt")
+val input1 = try inputFile
+                   .getLines
+                   .next
+             finally inputFile.close()
+
+val image = Image(input1, 25,6)
+
+
+val layers = for(i <- 0 until image.layerCount) yield (i, image.layer(i).filter(_ == 0).size)
+
+val layersSorted = layers.sortWith(_._2 < _._2)
+val layer = image.layer(layersSorted.head._1)
+val calc = layer.filter(_ == 1).size * layer.filter(_ == 2).size
+
+println(s"Part 1\n$calc")
+
