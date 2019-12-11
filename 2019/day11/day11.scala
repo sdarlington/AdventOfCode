924761eeaa6664640a0ad750842201d33b12b4be
Stephen Darlington <stephen@zx81.org.uk>
2019-12-11 20:41:24 +0000

    Day 11, parts 1 and 2

---
+++ 2019/day11/day11.scala
@@ -0,0 +1,253 @@
+
+import scala.collection.mutable.Buffer
+import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable.HashMap
+
+class VirtualMachine(memory : Buffer[Long]) {
+  import scala.collection.mutable.ListBuffer
+
+  def this(input : String) = this(input.split(",").map(_.toLong).toBuffer)
+
+  def runCalculation(stopOnOutput : Boolean = false) : Seq[Long] = {
+    // var memory = new VirtualMachine(input.toBuffer)
+    while (step && (!stopOnOutput || (stopOnOutput && outputQueue.size == 0))) {}
+    // allMemory
+    val retv = outputQueue.toSeq
+    outputQueue.clear
+    retv
+  }
+
+  val outputQueue = ListBuffer[Long]()
+  val inputQueue = ArrayBuffer[Long]()
+
+  var pc = 0
+  var relativeOffsetBase = 0
+
+  def incPC(inc : Int = 1) : Unit = pc += inc
+
+  def mapAddress(address : Int, mode : ParameterMode.Value) : Int = {
+      mode match {
+          case ParameterMode.Position => memory(address).toInt
+          case ParameterMode.Immediate => address
+          case ParameterMode.Relative => relativeOffsetBase + memory(address).toInt
+      }
+  }
+  def value(address : Int, mode : ParameterMode.Value = ParameterMode.Position) : Long = {
+      val addr = mapAddress(address,mode)
+      if (addr >= memory.size) {
+          memory.padToInPlace(addr + 10, 0)
+      }
+      memory(addr)
+  }
+
+  def setValue(address : Int, value : Long) : Unit = {
+      if (address >= memory.size) {
+          memory.padToInPlace(address + 10, 0)
+      }
+      memory(address) = value
+    }
+
+  def allMemory() : Array[Long] = memory.toArray[Long]
+
+  def printMemory() : Unit = println(memory.mkString(","))
+
+  def printCommands() : Unit = {
+      var c = 0
+      while (c < 200) {
+          val command = value(c, ParameterMode.Immediate)
+        val (instruction,modes) = instructionToOpcodeParameterMode(command)
+        print (s"$c  $instruction ($command) ")
+        for (i <- 1 to parameters(instruction).toInt) {
+            print (s" ${value(c+i,ParameterMode.Immediate)} ")
+        }
+        println()
+        c += parameters(instruction).toInt + 1
+      }
+  }
+
+  object ParameterMode extends Enumeration {
+      type ParameterMode = Value
+      val Position, Immediate, Relative = Value
+  }
+
+  def instructionToOpcodeParameterMode(instruction : Long) : (Long,Seq[ParameterMode.Value]) = {
+    val opcode = if (instruction < 100) { instruction  } else { instruction % 100 }
+    val modeCount = parameters(opcode)
+    val modesList = (instruction / 100)
+                   .toString
+                   .reverse
+    val modes = modesList
+                   .concat("0" * (modeCount.toInt - modesList.length))
+                   .map(_ match {
+                       case '0' => ParameterMode.Position
+                       case '1' => ParameterMode.Immediate
+                       case '2' => ParameterMode.Relative
+                   })
+    (opcode,modes)
+  }
+
+  def step() : Boolean = {
+      val (instruction,modes) = instructionToOpcodeParameterMode(value(pc, ParameterMode.Immediate))
+      val retv = opcode(instruction)(modes)
+      incPC(parameters(instruction).toInt + 1)
+      retv
+  }
+
+  val opcode = Instructions.values.toList.map(x => (x.opcode, x.code)).toMap
+  val parameters = Instructions.values.toList.map(x => (x.opcode, x.paramCount)).toMap
+
+  object Instructions extends Enumeration {
+    protected case class Val(opcode : Long, paramCount : Long, code : (Seq[ParameterMode.Value]) => Boolean) extends super.Val 
+
+    import scala.language.implicitConversions
+    implicit def valueToInstructionVal(x: Value): Val = x.asInstanceOf[Val]
+
+    val Add = Val(1, 3, (p) => {
+      setValue(mapAddress(pc + 3, p(2)), value(pc + 1, p(0)) + value(pc + 2, p(1)))
+      true
+    })
+    val Multiply = Val(2, 3, (p) => {
+      setValue(mapAddress(pc + 3, p(2)), value(pc + 1, p(0)) * value(pc + 2, p(1)))
+      true
+    })
+    val IntegerInput = Val(3, 1, (p) => {
+        // println("Enter integer: ")
+        val num = inputQueue.head
+        inputQueue.remove(0)
+        setValue(mapAddress(pc + 1, p(0)), num)
+        true
+    })
+    val LongegerOutput = Val(4, 1, (p) => {
+        outputQueue += value(pc + 1, p(0))
+        true
+    })
+    val JumpIfTrue = Val(5, 2, (p) => {
+        if (value(pc + 1, p(0)) != 0) {
+            // PC will be incremented by 3 on returm
+            pc = value(pc + 2, p(1)).toInt - 3
+        }
+        true
+    })
+    val JumpIfFalse = Val(6,2, (p) => {
+        if (value(pc + 1, p(0)) == 0) {
+            pc = value(pc + 2, p(1)).toInt - 3
+        }
+        true
+    })
+    val LessThan = Val(7, 3, (p) => {
+        setValue(mapAddress(pc + 3, p(2)),
+          if (value(pc + 1, p(0)) < value(pc + 2, p(1))) {
+            1
+          }
+          else {
+              0
+          })
+          true
+    })
+    val Equals = Val(8, 3, (p) => {
+        setValue(mapAddress(pc + 3, p(2)).toInt,
+          if (value(pc + 1, p(0)) == value(pc + 2, p(1))) {
+            1
+          }
+          else {
+              0
+          })
+          true
+    })
+    val UpdateRelativeOffset = Val(9, 1, (p) => {
+        relativeOffsetBase += value(pc + 1, p(0)).toInt
+        true
+    })
+    val Halt = Val(99, 0, (p) => false)
+  }
+
+}
+
+// Part 1
+val inputFile = io.Source.fromFile("input.txt")
+val input1 = try inputFile
+                   .getLines
+                   .next
+             finally inputFile.close()
+
+object Direction extends Enumeration {
+    type Direction = Value
+    val U, R, D, L = Value
+}
+
+def nextDirection(d : Direction.Value, turn : Int) : Direction.Value = {
+    d match {
+        case Direction.U => if (turn == 0) { Direction.L } else { Direction.R }
+        case Direction.R => if (turn == 0) { Direction.U } else { Direction.D }
+        case Direction.D => if (turn == 0) { Direction.R } else { Direction.L }
+        case Direction.L => if (turn == 0) { Direction.D } else { Direction.U }
+    }
+}
+
+def nextLocation(x : Int, y : Int, dir : Direction.Value) : (Int,Int) = {
+  dir match {
+      case Direction.U => (x, y + 1)
+      case Direction.R => (x + 1, y)
+      case Direction.D => (x, y - 1)
+      case Direction.L => (x - 1, y)
+  }
+}
+
+println("Part 1:")
+val vm = new VirtualMachine(input1)
+val robotMap = HashMap[(Int,Int),Int]()
+var x, y = 0
+var direction = Direction.U
+var running = true
+val startingColour = 0
+var started = false
+while (running) {
+  val inputColour = if (robotMap.contains((x,y))) {
+    robotMap((x,y))
+  }
+  else {
+    if (!started) {
+        started = true
+        startingColour
+    } else {
+        0
+    }
+  }
+  vm.inputQueue.append(inputColour)
+  val result = vm.runCalculation(true)
+  if (result.size == 0) {
+      val c = robotMap.size
+      println (c)
+      running = false
+  }
+  else {
+    val colour = result.head
+    val turn = vm.runCalculation(true).head
+    robotMap((x,y)) = colour.toInt
+    val nextDir = nextDirection(direction, turn.toInt)
+    direction = nextDir
+    val n = nextLocation(x, y, nextDir)
+    x = n._1
+    y = n._2
+  }
+}
+
+// Part 2
+println ("Part 2")
+val minX = robotMap.keys.map(_._1).min
+val minY = robotMap.keys.map(_._2).min
+val maxX = robotMap.keys.map(_._1).max
+val maxY = robotMap.keys.map(_._2).max
+
+for (y <- maxY to minY by -1) {
+    for (x <- minX to maxX) {
+      if (robotMap.contains((x,y))) {
+          print ( if (robotMap((x,y)) == 0) { "⬛" } else { "⬜" } )
+      }
+      else {
+          print ("⬛")
+      }
+    }
+    println()
+}
+
