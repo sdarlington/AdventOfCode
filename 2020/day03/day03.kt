1aa02d8642c84197706a1750cb9752fba2d78b91
Stephen Darlington <stephen@zx81.org.uk>
2020-12-13 17:24:11 +0000

    Day 3, part 1

---
+++ 2020/day03/day03.kt
@@ -0,0 +1,63 @@
+
+import java.io.File
+
+enum class Geology { TREE, OPEN }
+fun Geology(x : Char) : Geology = when (x) {
+    '#' -> Geology.TREE
+    else -> Geology.OPEN
+}
+
+class Map (val map: List<List<Geology>>) {
+    fun run1(dx : Int = 3, dy : Int = 1) : Int {
+        var x = 0
+        var y = 0
+        var treeCount = 0
+
+        while (y < map.count()) {
+            val isTree = geologyAt(x,y) == Geology.TREE
+            if (isTree) {
+                treeCount++
+            }
+
+            x += dx
+            y += dy
+        }
+
+        return treeCount
+    }
+
+    fun geologyAt(x : Int, y : Int) : Geology {
+        val row = map.getOrElse(y) { listOf() }
+        val space = row.getOrElse(x % row.count()) { Geology.OPEN }
+        return space
+    }
+
+    fun print() {
+        for (l in map) {
+            for (p in l) {
+                when (p) {
+                    Geology.TREE -> print("#")
+                    Geology.OPEN -> print(" ")
+                    }
+            }
+            println ()
+        }
+    }
+}
+
+fun Map(file : String) : Map {
+    val entries = File(file).readLines().map { x -> parseLine(x) }
+    return Map(entries)
+}
+
+fun parseLine(line : String) : List<Geology> = line.map { x -> Geology(x) }
+
+fun main() {
+    // Example
+    val map = Map("example.txt")
+     println (map.run1())
+
+     // Part 1
+    val map2 = Map("input.txt")
+     println (map2.run1())
+}
