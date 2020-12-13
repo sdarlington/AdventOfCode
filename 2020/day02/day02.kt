d4975a4a11ac4460aaca5c7e1be4d6860e85104d
Stephen Darlington <stephen@zx81.org.uk>
2020-12-13 15:55:31 +0000

    Tidy up day 2

--- 2020/day02/day02.kt
+++ 2020/day02/day02.kt
@@ -1,20 +1,22 @@
 
 import java.io.File
 
-class Password(val minCount : Int, val maxCount : Int, val letter : Char, val password : String) 
+class Password(val minCount : Int, val maxCount : Int, val letter : Char, val password : String) {
+    fun checkPassword() : Boolean {
+        val check = this.password.filter { x -> x == this.letter }.count()
+        return (check >= this.minCount && check <= this.maxCount)
+    }
 
-fun checkPassword(password : Password) : Boolean {
-    val check = password.password.filter { x -> x == password.letter }.count()
-    return (check >= password.minCount && check <= password.maxCount)
+    fun checkPassword2() : Boolean {
+        val p1 = this.password.getOrElse(this.minCount - 1) { '.' }
+        val p2 = this.password.getOrElse(this.maxCount - 1) { '.' }
+        return ((p1 == this.letter).xor(p2 == this.letter))
+    }
+
+    override fun toString() : String = "Password[minCount:$minCount,maxCount:$maxCount,letter:$letter,password:$password]"
 }
 
-fun checkPassword2(password : Password) : Boolean {
-    val p1 = password.password.getOrElse(password.minCount - 1) { '.' }
-    val p2 = password.password.getOrElse(password.maxCount - 1) { '.' }
-    return ((p1 == password.letter).xor(p2 == password.letter))
-}
-
-fun parseInput(line : String) : Password {
+fun Password(line : String) : Password {
     val components = line.split(" ")
     val counts = components.getOrElse(0) { "0-0" }
     val letter = components.getOrElse(1) { ":" }
@@ -29,23 +31,21 @@ fun main() {
     // Example
     val example = listOf(Password(1,3,'a',"abcde"), Password(1,3,'b',"cdefg"), Password(2,9,'c',"ccccccccc"))
     for (p in example) {
-        print (p)
-        print (" -> ")
-        println(checkPassword(p))
+        println ("$p -> ${p.checkPassword()}")
     }
 
     // Part 1
     val entries = File("input.txt").readLines()
-                                   .map { x -> parseInput(x) }
-                                   .map { x -> checkPassword(x) }
+                                   .map { x -> Password(x) }
+                                   .map { x -> x.checkPassword() }
                                    .filter { x -> x == true }
                                    .count()
     println (entries)
 
     // Part 2
     val entries2 = File("input.txt").readLines()
-                                    .map { x -> parseInput(x) }
-                                    .map { x -> checkPassword2(x) }
+                                    .map { x -> Password(x) }
+                                    .map { x -> x.checkPassword2() }
                                     .filter { x -> x == true }
                                     .count()
     println (entries2)
