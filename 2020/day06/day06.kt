f6f7c681746888a25b56f5ffb7680e657f82df93
Stephen Darlington <stephen@zx81.org.uk>
2020-12-19 14:18:12 +0000

    Day 6, part 1

---
+++ 2020/day06/day06.kt
@@ -0,0 +1,47 @@
+
+
+import java.io.File
+
+class Declarations() {
+    fun readFile(file : String) : List<List<List<Char>>> {
+        val entries = File(file).readLines()
+
+        val groups : MutableList<List<List<Char>>> = mutableListOf()
+        var people : MutableList<List<Char>> = mutableListOf()
+        for (line in entries) {
+            if (line.count() == 0) {
+                groups.add(people)
+                people = mutableListOf()
+            }
+            else {
+                people.add(line.toList())
+            }
+        }
+        if (people.count() > 0) {
+            groups.add(people)
+        }
+
+        return groups
+    }
+
+    fun uniqueAnswers(answers : List<List<Char>>) : Int {
+        return answers.map { x -> x.toSet() }
+                      .reduce { acc, v -> acc.union(v) }
+                      .count()
+    }
+}
+
+fun main() {
+    // sample
+    val decl = Declarations()
+    val sample = decl.readFile("example.txt")
+    val sampleCount = sample.map { x -> decl.uniqueAnswers(x) }
+                            .reduce { acc, v -> acc + v }
+    println (sampleCount)
+                          
+    println ("Part 1")
+    val input = decl.readFile("input.txt")
+    val step1Count = input.map { x -> decl.uniqueAnswers(x) }
+                          .reduce { acc, v -> acc + v }
+    println (step1Count)
+}
