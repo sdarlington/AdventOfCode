adae2176747bf7af81e83a49bd5adf904c73fcfe
Stephen Darlington <stephen@zx81.org.uk>
2021-12-02 09:15:02 +0000

    Day 2, part 1

---
+++ 2021/day02/day02.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+sample = [
+    "forward 5",
+    "down 5",
+    "forward 8",
+    "up 3",
+    "down 8",
+    "forward 2"
+]
+
+f = open("input.txt","r")
+puzzle = [ x for x in f.readlines()]
+
+# Part 1
+
+import enum
+class Instruction(enum.Enum):
+    Forward = 1
+    Down = 2
+    Up = 3
+
+    def parse(string):
+        lex = string.split(' ')
+        command = { 'forward' : Instruction.Forward, 'up' : Instruction.Up, 'down' : Instruction.Down }[lex[0]]
+        return (command, int(lex[1]))
+
+def nextLocation(location, instruction):
+    command = {
+        Instruction.Forward : lambda l,d: (l[0] + d, l[1]),
+        Instruction.Up      : lambda l,d: (l[0], l[1] - d),
+        Instruction.Down    : lambda l,d: (l[0], l[1] + d)
+        }
+    return command[instruction[0]](location, instruction[1])
+
+def part1(instructions):
+    location = (0,0)   
+    for i in instructions:
+        location = nextLocation(location, Instruction.parse(i))
+    return location
+
+print ("Part 1")
+location = part1(sample)
+print ("Sample ", location)
+print ("Sample ", location[0] * location[1])
+
+location = part1(puzzle)
+print ("Part 1 ", location)
+print ("Part 1 ", location[0] * location[1])
