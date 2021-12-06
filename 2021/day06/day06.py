faba10a286ec5b14bd07201fbfa52520697a6135
Stephen Darlington <stephen@zx81.org.uk>
2021-12-06 18:20:24 +0000

    Day 6, part 2

--- 2021/day06/day06.py
+++ 2021/day06/day06.py
@@ -7,21 +7,43 @@ puzzle = [[int(a) for a in x.replace('\n','').split(',')] for x in f.readlines()
 
 # part 1
 def nextDay(input, createFish = 7, newFish = 2):
+    output = {}
     for fish in input:
-        fish = fish - 1
-        if fish < 0:
-            fish = fish + createFish
-            yield createFish + newFish - 1
-        yield fish
+        new_fish = fish - 1
+        if new_fish < 0:
+            new_fish = new_fish + createFish
+            if createFish + newFish -1 in output:
+                v = output[createFish + newFish -1] + input[fish]
+            else:
+                v = input[fish]
+            output[createFish + newFish - 1] = v
+        if new_fish in output:
+            v = output[new_fish] + input[fish]
+        else:
+            v = input[fish]
+        output[new_fish] = v
+    return output
+
+def countFish(input):
+    return sum([input[x] for x in input ])
 
 def days(input, days):
-    # print ('Initial state: ', input)
+    pinput = {}
+    for f in input:
+        if f in pinput:
+            x = pinput[f] + 1
+        else:
+            x = 1
+        pinput[f] = x
     for d in range(0,days):
-        input = [x for x in nextDay(input)]
-        # print ('After ', d + 1, ' days: ', input)
-    return input
+        pinput = nextDay(pinput)
+    return countFish(pinput)
 
 print ('Part 1')
-print (len(days(sample,18)))
-print (len(days(sample,80)))
-print (len(days(puzzle,80)))
+print (days(sample,18))
+print (days(sample,80))
+print (days(puzzle,80))
+
+# print ('Part 2')
+print (days(sample,256))
+print (days(puzzle,256))
