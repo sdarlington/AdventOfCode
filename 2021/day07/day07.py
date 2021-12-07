0661c0edeaf5c82c68e5189acb7f3651fbedba3a
Stephen Darlington <stephen@zx81.org.uk>
2021-12-07 09:05:38 +0000

    Day 7, part 2

--- 2021/day07/day07.py
+++ 2021/day07/day07.py
@@ -10,10 +10,10 @@ puzzle = [[int(a) for a in x.replace('\n','').split(',')] for x in f.readlines()
 def alignTo(positions,align):
     return [ abs(x - align) for x in positions ]
 
-def calcMinFuel(positions):
+def calcMinFuel(positions, fuelCalc = alignTo):
     minFuel = (None,None)
     for p in range(min(positions),max(positions)+1):
-        fuel = sum(alignTo(positions,p))
+        fuel = sum(fuelCalc(positions,p))
         if minFuel[0] is None or fuel < minFuel[1]:
             minFuel = (p,fuel)
     return minFuel
@@ -22,3 +22,12 @@ print (sum(alignTo(sample,2)))
 print (calcMinFuel(sample))
 
 print (calcMinFuel(puzzle))
+
+# Part 2
+def alignTo2(positions,align):
+    return [ (lambda x: int((x * (x+1)) / 2))(abs(x - align)) for x in positions]
+
+print (sum(alignTo2(sample,5)))
+print (calcMinFuel(sample,alignTo2))
+
+print (calcMinFuel(puzzle, alignTo2))
