2252f4a3139b755c38d88a54285ef974476bdb81
Stephen Darlington <stephen@zx81.org.uk>
2021-12-10 10:38:00 +0000

    Day 10, part 2

--- 2021/day10/day10.py
+++ 2021/day10/day10.py
@@ -19,12 +19,31 @@ def parse(line):
             opposite = { ')':'(', ']':'[', '}':'{', '>':'<' }[e]
             if match != opposite:
                 return (e,match)
-    return None
+    return (None,stack)
 
 def part1(input):
     results = [parse(x) for x in input]
-    cal = [{')':3, ']':57, '}':1197, '>': 25137}[x[0]] for x in results if x is not None]
+    cal = [{')':3, ']':57, '}':1197, '>': 25137}[x[0]] for x in results if x[0] is not None]
     return sum(cal)
 
 print(part1(sample))
 print(part1(puzzle))
+
+# Part 2
+from functools import reduce
+
+def part2(input):
+    results = []
+    for l in input:
+        (last,stack) = parse(l)
+        if last is None:
+            stack.reverse()
+            score = [ {'(':1, '[':2, '{':3, '<':4}[x] for x in stack]
+            total = reduce(lambda x,y: x*5 + y, score)
+            results.append(total)
+    results.sort()
+    return results[int(len(results) / 2)]
+
+print ('\nPart 2')
+print(part2(sample))
+print(part2(puzzle))
