7b6e6fe4f481cb96d099a693dd24719adf83daaf
Stephen Darlington <stephen@zx81.org.uk>
2021-12-12 19:20:14 +0000

    Day 12, part 2

--- 2021/day12/day12.py
+++ 2021/day12/day12.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from functools import reduce
 
 f = open("sample.txt","r")
 sample = [ x.replace('\n','') for x in f.readlines()]
@@ -34,20 +35,30 @@ class CaveMap:
     def get_paths(self):
         return self.paths
 
-    def find_paths(self, start, path = []):
+    def find_paths(self, start, visit, path = []):
         path.append(start)
         if start in self.map:
             for p in self.map[start]:
-                if p not in path or p.upper() == p:
+                if visit(p,path) or p.upper() == p:
                     if p == 'end':
                         n = path.copy()
                         n.append(p)
                         self.paths.append(n)
                     else:
-                        self.find_paths(p,path.copy())
+                        self.find_paths(p, visit, path.copy())
 
     def part1(self):
-        self.find_paths('start')
+        self.find_paths('start', lambda x,y: x not in y)
+        return len(self.get_paths())
+
+    def part2(self):
+        def check(cave,path):
+            counts = {}
+            for c in [x for x in path if x.lower() == x]:
+                counts[c] = 1 if c not in counts else counts[c] + 1
+            doubles = len([x for x in counts if counts[x] == 2])
+            return cave not in counts or (counts[cave] == 1 and doubles == 0)
+        self.find_paths('start', check )
         return len(self.get_paths())
 
 print ('Part 1')
@@ -59,3 +70,13 @@ print (sample_map.part1())
 
 puzzle_map = CaveMap(puzzle)
 print (puzzle_map.part1())
+
+print ('Part 2')
+sample_map = CaveMap(sample)
+print (sample_map.part2())
+
+sample_map = CaveMap(sample2)
+print (sample_map.part2())
+
+puzzle_map = CaveMap(puzzle)
+print (puzzle_map.part2())
