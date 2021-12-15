d6d8404608fac51407f721d179f0c5bd020d4bcf
Stephen Darlington <stephen@zx81.org.uk>
2021-12-15 08:17:09 +0000

    Day 14, faster but not fast enough for part 2

--- 2021/day14/day14.py
+++ 2021/day14/day14.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 from collections import Counter
-from functools import reduce
+# from functools import reduce
+# from collections import deque 
+import cProfile
 
 f = open("sample.txt","r")
 sample = [ x.replace('\n','') for x in f.readlines()]
@@ -19,29 +21,34 @@ def parse(input):
             template = l
         elif l != '':
             (elA,elB) = l.split(' -> ')
-            rules[elA] = elB
+            el = list(elA)
+            rules[(el[0],el[1])] = elB
     return (template,rules)
 
-def apply(input,rules):
-    out = []
-    elements = list(input)
+def apply(elements,rules):
+    out = [ 0 ] * (len(elements) * 2 - 1) # deque()
     for x in range(0,len(elements)-1):
-        (a,b) = elements[x:x+2]
-        out.append(a)
-        k = ''.join([a,b])
-        if k in rules:
-            out.append(rules[k])
-    out.append(elements[-1])
-    return ''.join(out)
+        ab = (elements[x],elements[x+1])
+        out[x*2],out[x*2+1] = ab[0], rules[ab]
+    out[len(elements) * 2 - 2] = elements[-1]
+    return out
 
 def part1(input,iterate):
     (template,rules) = parse(input)
-    val = template
+    val = template # deque(template)
     for i in range(0,iterate):
+        if i % 5 == 0:
+            print (i, ' ', len(val))
         val = apply(val,rules)
-    calc = Counter(list(val))
+    calc = Counter(val)
     return calc.most_common()[0][1] - calc.most_common()[-1][1]
 
 print ('Part 1')
 print (part1(sample,10))
 print (part1(puzzle,10))
+
+# cProfile.run("part1(puzzle,20)")
+
+print ('Part 2')
+print (part1(sample,40))
+# print (part1(puzzle,40))
